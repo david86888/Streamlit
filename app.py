39b2f21efc27108c5c3a760b65b818ad9ed7218f
Siang-Li Jheng <johnny.mg11@nycu.edu.tw>
2024-08-30 11:33:50 +0000

    Upload file

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# 20240830
+import streamlit as st
+import Results
+import Table
+
+st.title("FRM@Taiwan")
+
+if st.button('Score Results'):
+    Results.show_results()
+
+if st.button('Table Results'):
+    Table.show_table()
